8227831fdd74bffd8e4514a894a063c9a8e8e38f
Brunno Quadrado <bbertoninuti@gmail.com>
2025-03-15 13:10:34 -0300

    main activity

--- app/src/main/java/com/quadrado/testeapi/MainActivity.kt
+++ app/src/main/java/com/quadrado/testeapi/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.quadrado.testeapi
 
 import android.os.Bundle
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val listaUsuarios = findViewById<ListView>(R.id.lista)
+        val fab_usuario = findViewById<FloatingActionButton>(R.id.fab_novoUsuario)
+
     }
 }
